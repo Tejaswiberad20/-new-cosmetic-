583296de7a48883533acc05358db7824d0df3cb9
Tejaswiberad20 <tejaswiberad2004@gmail.com>
2024-06-15 19:31:51 +0530

    login

--- js/index.js
+++ js/index.js
@@ -4,9 +4,10 @@ function like(){
     const like = document.getElementById('s1');
     {
         like.src = './imges/heart.png';
-        
+    
     }
   }
+ 
 function addtocard(){
     alert('Product Add to your card');
 }
